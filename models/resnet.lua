192ffb741208a0533c7ccd397f64c2a64bf5ca03
Anirudhan J. Rajagopalan <ajr619@nyu.edu>
2016-07-06 23:16:47 -0700

    resnet model

--- models/resnet.lua
+++ models/resnet.lua
@@ -31,7 +31,126 @@ local function createModel(opt)
         end
     end
 
+    local function basicblock(n, stride)
+        local nInputPlane = iChannels
+        iChannels = n
 
+        local s = nn.Sequential()
+        s:add(Convolution(nInputPlane, n, 3, 3, stride, stride, 1, 1))
+        s:add(SBatchNorm(n))
+        s:add(ReLU(true))
+        s:add(Convolution(n,n,3,3,1,1,1,1))
+        s:add(SBatchNorm(n))
+
+        return nn.Sequential()
+            :add(nn.ConcatTable()
+                :add(s)
+                :add(shortcut(nInputPlane, n, stride)))
+            :add(nn.CAddTable(true))
+            :add(ReLU(true))
+    end
+
+    local function bottleneck(n, stride)
+        local nInputPlane = iChannels
+        iChannels = n * 4
+
+        local s = nn.Sequential()
+        s:add(Convolution(nInputPlane,n,1,1,1,1,0,0))
+        s:add(SBatchNorm(n))
+        s:add(ReLU(true))
+        s:add(Convolution(n,n,3,3,stride,stride,1,1))
+        s:add(SBatchNorm(n))
+        s:add(ReLU(true))
+        s:add(Convolution(n,n*4,1,1,1,1,0,0))
+        s:add(SBatchNorm(n * 4))
+
+        return nn.Sequential()
+            :add(nn.ConcatTable()
+                :add(s)
+                :add(shortcut(nInputPlane, n * 4, stride)))
+            :add(nn.CAddTable(true))
+            :add(ReLU(true))
+    end
+
+    -- Creates count residual blocks with specified number of features
+    local function layer(block, features, count, stride)
+        local s = nn.Sequential()
+        for i=1,count do
+         s:add(block(features, i == 1 and stride or 1))
+        end
+        return s
+    end
+
+    local model = nn.Sequential()
+      local cfg = {
+         [18]  = {{2, 2, 2, 2}, 512, basicblock},
+         [34]  = {{3, 4, 6, 3}, 512, basicblock},
+         [50]  = {{3, 4, 6, 3}, 2048, bottleneck},
+         [101] = {{3, 4, 23, 3}, 2048, bottleneck},
+         [152] = {{3, 8, 36, 3}, 2048, bottleneck},
+      }
+
+      assert(cfg[depth], 'Invalid depth: ' .. tostring(depth))
+      local def, nFeatures, block = table.unpack(cfg[depth])
+      iChannels = 64
+      print(' | ResNet-' .. depth .. ' ImageNet')
+
+      -- The ResNet ImageNet model
+      model:add(Convolution(3,64,7,7,2,2,3,3))
+      model:add(SBatchNorm(64))
+      model:add(ReLU(true))
+      model:add(Max(3,3,2,2,1,1))
+      model:add(layer(block, 64, def[1]))
+      model:add(layer(block, 128, def[2], 2))
+      model:add(layer(block, 256, def[3], 2))
+      model:add(layer(block, 512, def[4], 2))
+      model:add(Avg(7, 7, 1, 1))
+      model:add(nn.View(nFeatures):setNumInputDims(3))
+      model:add(nn.Linear(nFeatures, 1000))
+
+
+    local function ConvInit(name)
+        for k,v in pairs(model:findModules(name)) do
+            local n = v.kW*v.kH*v.nOutputPlane
+            v.weight:normal(0,math.sqrt(2/n))
+            if cudnn.version >= 4000 then
+                v.bias = nil
+                v.gradBias = nil
+            else
+                v.bias:zero()
+            end
+        end
+    end
+
+    local function BNInit(name)
+        for k,v in pairs(model:findModules(name)) do
+            v.weight:fill(1)
+            v.bias:zero()
+        end
+    end
+
+    ConvInit('cudnn.SpatialConvolution')
+    ConvInit('nn.SpatialConvolution')
+    BNInit('fbnn.SpatialBatchNormalization')
+    BNInit('cudnn.SpatialBatchNormalization')
+    BNInit('nn.SpatialBatchNormalization')
+
+    for k,v in pairs(model:findModules('nn.Linear')) do
+        v.bias:zero()
+    end
+    model:cuda()
+
+    if opt.cudnn == 'deterministic' then
+        model:apply(function(m)
+        if m.setMode then 
+            m:setMode(1,1,1) 
+        end
+        end)
+    end
+
+    model:get(1).gradInput = nil
+
+    return model
 end
 
 return createModel
