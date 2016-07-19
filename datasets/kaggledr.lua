fe1e53b380dfce2330d99dfcc1d0e47c29400ecc
Anirudhan J. Rajagopalan <ajr619@nyu.edu>
2016-07-18 17:01:41 -0700

    changes for implementing resampling

--- datasets/kaggledr.lua
+++ datasets/kaggledr.lua
@@ -5,11 +5,14 @@ local M = {}
 
 local KaggleDR = torch.class('eyeart.KaggleDR', M)
 
+local DR_LEVELS = {0, 1, 2, 3, 4}
+
 
 function KaggleDR:__init(opt, split, info)
     self.split = split
     self.dir = opt.data
     self.info = info
+    self.data = info.data
     if split == 'train' then
         self.classDistribution = info.classDistribution
     end
@@ -18,6 +21,16 @@ function KaggleDR:__init(opt, split, info)
 
     self.initialWeights = torch.Tensor({1.36, 14.4, 6.64, 40.2, 49.6})
     self.finalWeights = torch.Tensor({1, 2, 2, 2, 2})
+
+    self.indices = torch.linspace(1, self.data:size(1), self.data:size(1))
+
+    self.classIndices = {}
+
+    for i, level in ipairs(DR_LEVELS) do
+        self.classIndices[level] = self.indices[self.data[{{}, {3}}]:eq(level)]
+    end
+
+    print('classIndices are ', split, self.classIndices, self.data:size())
 end
 
 function KaggleDR:get(id)
@@ -46,16 +59,32 @@ function KaggleDR:get_image_indicies(epoch)
     end
 
     -- do this only for training set
-
+    --
     local weights = self.R^(epoch -1) * self.initialWeights + (1 - self.R^(epoch - 1)) * self.finalWeights
     weights = torch.cmul(weights, self.classDistribution)
-    print(weights)
 
-    dr_levels = torch.multinomial(weights, self.__size, true)
+    dr_levels = torch.histc(torch.multinomial(weights, self.__size, true):double(), 5, 1, 5):totable()
+    print(dr_levels)
+    local indices
+    for level, count in pairs(dr_levels) do
+        local curIndices = self.classIndices[level -1]
+        local shuffle = torch.randperm(curIndices:size(1)):long()
+        curIndices = curIndices:index(1, shuffle)
+        local mul_factor = math.ceil(count/curIndices:size(1))
+        print('mul_factor is ', mul_factor)
 
-    for i = 1, 5 do print(dr_levels:eq(i):sum()) end
+        
 
-    return torch.randperm(self.__size)
+        if not indices then
+            indices = curIndices:repeatTensor(mul_factor)[{{1,count}}]
+            print('current indices size is ', indices:size())
+        else
+            indices = torch.cat(indices, curIndices:repeatTensor(mul_factor)[{{1,count}}], 1)
+        end
+    end
+    print('final indices size is ', indices:size())
+
+    return indices
 end
 
 function KaggleDR:preprocess(img)
